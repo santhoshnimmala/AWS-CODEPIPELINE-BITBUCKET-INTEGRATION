b1bbc7a16e2353560c7c1797e693515cde0f85b3
santhoshnimmala <nsanthosh.1993@gmail.com>
2022-11-15 16:39:46 +0000

    Update Convert.py

--- Convert.py
+++ Convert.py
@@ -1,3 +1,9 @@
+import os
+import subprocess
+
+cwd = os.getcwd()  # Get the current working directory (cwd)
+files = os.listdir(cwd)  # Get all the files in that directory
+print("Files in %r: %s" % (cwd, files))
 # Using readlines()
 file1 = open('sample.txt','r')
 Lines = file1.readlines()
@@ -9,3 +15,7 @@ for line in Lines:
     d["value"]= line.rstrip('\n').split("=")[1]
     a.append(d)
 print(a)
+cmd = "aws servicecatalog provision-product --product-id prod-fhg67bjrz2lfq --provisioned-product-name 'mytestppname3' --provisioning-parameters {}".format(a)
+c=subprocess.call(cmd, shell=True)
+
+
