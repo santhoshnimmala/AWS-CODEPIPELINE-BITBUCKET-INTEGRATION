7fcc446637777be342801a2d68bf1c357be154a8
santhoshnimmala <nsanthosh.1993@gmail.com>
2022-11-16 14:54:56 +0000

    Update Convert.py

--- Convert.py
+++ Convert.py
@@ -13,8 +13,8 @@ client = boto3.client('servicecatalog',region_name='us-east-1')
 # Strips the newline character
 for line in Lines:
     d = {}
-    d["key"]=line.rstrip('\n').split("=")[0]
-    d["value"]= line.rstrip('\n').split("=")[1]
+    d["Key"]=line.rstrip('\n').split("=")[0]
+    d["Value"]= line.rstrip('\n').split("=")[1]
     a.append(d)
 print(a)
 cmd = "aws servicecatalog provision-product --product-id prod-fhg67bjrz2lfq --provisioned-product-name 'mytestppname3' --provisioning-parameters {}".format(a)
